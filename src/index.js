27ac5bc9858bc2141a8dcfe75069a93a4d01e029
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-09-30 13:18:36 +0200

    Exercise concluded.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-import './styles/main.scss'; // Don't delete :)
-import smile from './scripts/smile.js'; // Feel free to delete :)
+import "./styles/main.scss";
 
-// All of your javascript should go here :)
+const newYear = new Date("1 January 2023");
 
+const days = 1000 * 60 * 60 * 24;
+const hours = 1000 * 60 * 60;
+const minutes = 1000 * 60;
+const seconds = 1000;
 
-smile() // Feel free to delete :)
+function countDown() {
+  const now = Date.now(); // ms from 1/1/1970
+  const diff = newYear - now;
+
+  const daysLeft = ("0" + Math.floor(diff / days)).slice(-2);
+  const hoursLeft = ("0" + Math.floor((diff % days) / hours)).slice(-2);
+  const minutesLeft = (
+    "0" + Math.floor(((diff % days) % hours) / minutes)
+  ).slice(-2);
+  const secondsLeft = (
+    "0" + Math.floor((((diff % days) % hours) % minutes) / seconds)
+  ).slice(-2);
+
+  const spanDays = (document.querySelector("#days").textContent = daysLeft);
+  const spanHours = (document.querySelector("#hours").textContent = hoursLeft);
+  const spanMinutes = (document.querySelector("#minutes").textContent =
+    minutesLeft);
+  const spanSeconds = (document.querySelector("#seconds").textContent =
+    secondsLeft);
+
+  console.log("🚀 ~ days", daysLeft, hoursLeft, minutesLeft, secondsLeft);
+}
+
+const interval = setInterval(countDown, 1000);
